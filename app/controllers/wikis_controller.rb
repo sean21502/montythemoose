713f84b6c2f3a0daaa856380518bb673b0df6161
Sean Hite <seanhite@gmail.com>
2017-09-27 20:35:09 -0400

    error when creating private wikis by premium user

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -64,6 +64,6 @@ class WikisController < ApplicationController
 private
 
   def wiki_params
-    params.require(:wiki).permit(:title, :body)
+    params.require(:wiki).permit(:title, :body, :private)
   end
 end
