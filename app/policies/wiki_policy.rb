eac0fa09de9e6745c595646147977b0164c99a70
Sean Hite <seanhite@gmail.com>
2017-09-30 20:12:35 -0400

    Error on localhost:3000/wikis

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,15 +1,49 @@
-class Scope #< ApplicationPolicy.rb
+class WikiPolicy #< ApplicationPolicy.rb
     # you can inherit the initilaized objects from application policy. you could then remove all the instance variables and use the variables in ApplicationPolicy.rb
   attr_reader :current_user, :model
 # 'current_user' can be named anything, the object passed as 'current_user' when initialized is an activerecord 'user object' that devise supplies. it references the current_user, the model is a reference to the object that is being authorized.
-
-  def initialize(user, scope)
-    @user = user
-    @scope = scope
+  def initialize(current_user, model)
+    @current_user = current_user
+    @model = model
   end
 
-  def resolve
-    wikis = []
+  def index?
+    !!@current_user
+  end
+
+  def new?
+    !!@current_user
+  end
+
+  def create?
+    !!@current_user
+  end
+
+  def edit?
+    @model.user == @current_user || @current_user.admin?
+  end
+
+  def update?
+    @model.user == @current_user || @current_user.admin?
+  end
+
+  def destroy?
+    @model.user == @current_user || @current_user.admin?
+  end
+
+  def show?
+    !!@current_user
+  end
+
+  class Scope
+    
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      wikis = []
       if user.role == 'admin'
         wikis = scope.all
       elsif user.role = 'premium'
@@ -29,5 +63,6 @@ class Scope #< ApplicationPolicy.rb
         end
       end
       wikis
+    end
   end
 end
